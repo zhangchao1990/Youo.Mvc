db80591575f5d090c4c760a7d177feb593b2b4e6
zc <zc@ZHANGCHAO_GS>
2017-01-22 14:28:35 +0800

    111

---
+++ src/Youo.Domain/IRepositories/IDepartmentRepository.cs
@@ -0,0 +1,12 @@
+﻿using Youo.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Youo.Domain.IRepositories
+{
+    public interface IDepartmentRepository : IRepository<Department, Guid>
+    {
+    }
+}
--- src/Youo.MvcCore/Startup.cs
+++ src/Youo.MvcCore/Startup.cs
@@ -1,21 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.FileProviders;
+using System.IO;
 
 namespace Youo.MvcCore
 {
     public class Startup
     {
+        public IConfigurationRoot Configuration { get; }
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+            //初始化映射关系
+           // FonourMapper.Initialize();
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //获取数据库连接字符串
+            var sqlConnectionString = Configuration.GetConnectionString("Default");
+
+            ////添加数据上下文
+            //services.AddDbContext<FonourDbContext>(options => options.UseNpgsql(sqlConnectionString));
+            ////依赖注入
+            //services.AddScoped<IUserRepository, UserRepository>();
+            //services.AddScoped<IUserAppService, UserAppService>();
+            //services.AddScoped<IMenuRepository, MenuRepository>();
+            //services.AddScoped<IMenuAppService, MenuAppService>();
+            //services.AddScoped<IDepartmentRepository, DepartmentRepository>();
+            //services.AddScoped<IDepartmentAppService, DepartmentAppService>();
+            //services.AddScoped<IRoleRepository, RoleRepository>();
+            //services.AddScoped<IRoleAppService, RoleAppService>();
+            services.AddMvc();
+            //Session服务
+            //services.AddSession();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,13 +52,32 @@ namespace Youo.MvcCore
 
             if (env.IsDevelopment())
             {
+                //开发环境异常处理
                 app.UseDeveloperExceptionPage();
             }
-
-            app.Run(async (context) =>
+            else
             {
-                await context.Response.WriteAsync("Hello World!");
+                //生产环境异常处理
+                app.UseExceptionHandler("/Shared/Error");
+            }
+            //使用静态文件
+            app.UseStaticFiles();
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                FileProvider = new PhysicalFileProvider(Directory.GetCurrentDirectory())
             });
+
+            //Session
+            //app.UseSession();
+            //使用Mvc，设置默认路由为系统登录
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Login}/{action=Index}/{id?}");
+            });
+
+            //SeedData.Initialize(app.ApplicationServices); //初始化数据
         }
     }
 }
