be0c7db2535f320e2cf1d5841bd7db7c7c665d3c
zc <zc@ZHANGCHAO_GS>
2017-01-24 09:14:41 +0800

    youo.app

--- src/Youo.App/YouoMapper.cs
+++ src/Youo.App/YouoMapper.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Youo.App.DepartmentApp.Dtos;
 using Youo.App.MenuApp.Dtos;
+using Youo.App.RoleApp.Dtos;
 using Youo.App.UserApp.Dtos;
 using Youo.Domain;
 
